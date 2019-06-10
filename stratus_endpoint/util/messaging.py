83023962f6d2effb40b72aa4507f5df4b70bf706
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-06-10 13:03:34 -0400

    Add messaging

--- stratus_endpoint/util/messaging.py
+++ stratus_endpoint/util/messaging.py
@@ -1,11 +1,14 @@
 from stratus_endpoint.handler.base import Status
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Optional
+import traceback
 
 class ErrorRecord:
 
     def __init__(self, message: str, traceback: List[str]):
         self.message = message
         self.traceback = traceback
+
+    def
 
 class RequestMetadata:
 
@@ -15,10 +18,17 @@ class RequestMetadata:
         self._status = Status.IDLE
 
     def setError(self, message, traceback=None):
-        self._error = ErrorRecord( message, traceback )
+        self.setErrorRecord( ErrorRecord( message, traceback ) )
+
+    def setErrorRecord(self, eRec: ErrorRecord ):
+        self._error = eRec
+        self._status = Status.ERROR
 
     def setStatus(self, status: Status ):
         self._status = status
+
+    def setException(self, ex: Exception ):
+        self.setError(  getattr(ex, 'message', repr(ex)), traceback.format_tb(ex.__traceback__) )
 
     def addMessage( self, message ):
         self._messages.append( message )
