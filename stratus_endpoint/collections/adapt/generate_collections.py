0b83773729854d86becfd99e998f0196bd6dd2e7
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-07 17:45:46 -0500

    Fix base dir

--- stratus_endpoint/collections/adapt/generate_collections.py
+++ stratus_endpoint/collections/adapt/generate_collections.py
@@ -7,14 +7,15 @@ regen_colls = [ "merra2", "cip" ]
 base_dir = "/nfs4m/css/curated01"
 coll_dir = "/att/pubrepo/ILAB/data/collections/agg"
 
+if "cip" in regen_colls:
+    for collDir in glob(f"{base_dir}/create-ip/data/reanalysis/*"):
+        collName = os.path.basename(collDir).lower()
+        scanner2 = FileScanner( f"cip.{collName}", path=collDir, ext="nc" )
+        scanner2.write( coll_dir )
+
 if "merra2" in regen_colls:
     for collDir in glob(f"{base_dir}/merra2/data/*"):
         collName = os.path.basename(collDir).lower()
         scanner2 = FileScanner( f"merra2.{collName}", path=collDir, ext="nc4" )
         scanner2.write( coll_dir )
 
-if "cip" in regen_colls:
-    for collDir in glob(f"{base_dir}/create-ip/data/reanalysis/*"):
-        collName = os.path.basename(collDir).lower()
-        scanner2 = FileScanner( f"cip.{collName}", path=collDir, ext="nc" )
-        scanner2.write( coll_dir )
