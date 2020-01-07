c02f4212e42c5deba4f9703f61a93ba21fcd487d
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-07 16:46:19 -0500

    Add generate collections script

--- stratus_endpoint/collections/adapt/generate_collections.py
+++ stratus_endpoint/collections/adapt/generate_collections.py
@@ -2,10 +2,19 @@ from stratus_endpoint.collections.scan import FileScanner
 from glob import glob
 import os
 
+regen_colls = [ "cip"]
+
 base_dir = "/nfs4m/css/curated01"
+coll_dir = "/att/pubrepo/ILAB/data/collections/agg"
 
-for collDir in glob(f"{base_dir}/merra2/data/*"):
-    collName = os.path.basename(collDir).lower()
-    scanner2 = FileScanner( f"merra2.{collName}", path=collDir, ext="nc4" )
-    scanner2.write( "/att/pubrepo/ILAB/data/collections/agg" )
+if "merra2" in regen_colls:
+    for collDir in glob(f"{base_dir}/merra2/data/*"):
+        collName = os.path.basename(collDir).lower()
+        scanner2 = FileScanner( f"merra2.{collName}", path=collDir, ext="nc4" )
+        scanner2.write( coll_dir )
 
+if "cip" in regen_colls:
+    for collDir in glob(f"{base_dir}/create-ip/data/reanalysis/*"):
+        collName = os.path.basename(collDir).lower()
+        scanner2 = FileScanner( f"cip.{collName}", path=collDir, ext="nc" )
+        scanner2.write( coll_dir )
