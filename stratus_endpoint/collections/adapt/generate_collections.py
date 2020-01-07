ea656ba7f73e09113cd5dfd15d22530a325979cc
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-07 17:44:04 -0500

    Fix base dir

--- stratus_endpoint/collections/adapt/generate_collections.py
+++ stratus_endpoint/collections/adapt/generate_collections.py
@@ -2,7 +2,7 @@ from stratus_endpoint.collections.scan import FileScanner
 from glob import glob
 import os
 
-regen_colls = [ "cip"]
+regen_colls = [ "merra2", "cip" ]
 
 base_dir = "/nfs4m/css/curated01"
 coll_dir = "/att/pubrepo/ILAB/data/collections/agg"
