6a22a8d9c6a6011cd9ff3510dccb930ea21e98e2
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-07 16:18:05 -0500

    Add generate collections script

--- stratus_endpoint/collections/adapt/generate_collections.py
+++ stratus_endpoint/collections/adapt/generate_collections.py
@@ -6,6 +6,6 @@ base_dir = "/nfs4m/css/curated01"
 
 for collDir in glob(f"{base_dir}/merra2/data/*"):
     collName = os.path.basename(collDir)
-    scanner2 = FileScanner( f"merra2.{collName}", path=collDir, ext="nc2" )
+    scanner2 = FileScanner( f"merra2.{collName}", path=collDir, ext="nc4" )
     scanner2.write( "/att/pubrepo/ILAB/data/collections/agg" )
 
