b0ca165c71e6b0fa8dcb94b0f1d00dbf3ece3cb1
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-08 13:24:11 -0500

    Update generate collections

--- stratus_endpoint/collections/adapt/generate_collections.py
+++ stratus_endpoint/collections/adapt/generate_collections.py
@@ -1,21 +1,33 @@
 from stratus_endpoint.collections.scan import FileScanner
+from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional, Iterable, Union
 from glob import glob
-import os
+import os, time, math
+import multiprocessing as mp
+from multiprocessing import Pool
 
 regen_colls = [ "merra2", "cip" ]
 
 base_dir = "/nfs4m/css/curated01"
 coll_dir = "/att/pubrepo/ILAB/data/collections/agg"
 
-if "cip" in regen_colls:
-    for collDir in glob(f"{base_dir}/create-ip/data/reanalysis/*"):
-        collName = os.path.basename(collDir).lower()
-        scanner2 = FileScanner( f"cip.{collName}", path=collDir, ext="nc" )
-        scanner2.write( coll_dir )
+collection_specs = [
+    { "cip-merra2": "create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/*" },
+    { "cip-merra2-asm": "create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/*" },
+    { "merra2": "/merra2/data/*" }
+]
 
-if "merra2" in regen_colls:
-    for collDir in glob(f"{base_dir}/merra2/data/*"):
-        collName = os.path.basename(collDir).lower()
-        scanner2 = FileScanner( f"merra2.{collName}", path=collDir, ext="nc4" )
-        scanner2.write( coll_dir )
+def process_collection( collection_spec: Dict ):
+    for collId, file_paths in collection_spec.items():
+        for collDir in glob(f"{base_dir}/{file_paths}"):
+            collName = os.path.basename(collDir).lower()
+            scanner2 = FileScanner( f"{collId}-{collName}", path=collDir, ext="nc" )
+            scanner2.write( coll_dir )
 
+
+t0 = time.time()
+nproc = 2*mp.cpu_count()
+with Pool(processes=nproc) as pool:
+    pool.map( process_collection, collection_specs )
+
+
+print( f"Completed collection generation in {(time.time()-t0)/60.0} minutes")
