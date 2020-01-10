01ca0815b15fa14a99071bec4884664c48953e41
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-10 12:09:33 -0500

    Update generate collections

--- stratus_endpoint/collections/adapt/generate_collections.py
+++ stratus_endpoint/collections/adapt/generate_collections.py
@@ -9,12 +9,12 @@ base_dir = "/nfs4m/css/curated01"
 coll_dir = "/att/pubrepo/ILAB/data/collections/agg"
 
 collection_specs0 = [
-    { "cip_merra2": "create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/*" },
-    { "cip_merra2_asm": "create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/*" },
     { "merra2": "/merra2/data/*" }
 ]
 
 collection_specs1 = [
+    { "cip_merra2": "create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/*" },
+    { "cip_merra2_asm": "create-ip/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/*" },
     { "noaa_ncep_cfsr": "create-ip/data/reanalysis/NOAA-NCEP/CFSR/*" },
     { "noaa_ncep_mom3": "create-ip/data/reanalysis/NOAA-NCEP/MOM3/*" },
     { "noaa_gfdl_mom4": "create-ip/data/reanalysis/NOAA-GFDL/MOM4/ECDAv31/*"},
@@ -34,9 +34,7 @@ collection_specs1 = [
     { "uh-mitgcm_gecco2": "create-ip/data/reanalysis/University-Hamburg/MITgcm/GECCO2/*"}
 ]
 
-collection_specs2 = [
-    { "merra2": "/merra2/data/M2T1NXINT*" }
-]
+
 
 def process_collection( collection_spec: Dict ):
     for collId, file_paths in collection_spec.items():
@@ -47,7 +45,7 @@ def process_collection( collection_spec: Dict ):
 
 t0 = time.time()
 
-for collection_spec in collection_specs2:
+for collection_spec in collection_specs0:
     process_collection(collection_spec)
 
 print( f"Completed collection generation in {(time.time()-t0)/60.0} minutes")
