99ab54cec98d31d6c0d4278e5b070c3c73b54845
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-10 14:02:48 -0500

    Add file tests

--- stratus_endpoint/collections/adapt/file_tests/test_aviris_files.py
+++ stratus_endpoint/collections/adapt/file_tests/test_aviris_files.py
@@ -1,6 +1,6 @@
 import os, xarray, time, math
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
-from collection_intake.util.fileTest import FileTester
+from stratus_endpoint.util.fileTest import FileTester
 
 base_dirs = "/att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG/data/*"
 suffix = "_img"
--- stratus_endpoint/collections/adapt/file_tests/test_merra2_files.py
+++ stratus_endpoint/collections/adapt/file_tests/test_merra2_files.py
@@ -1,6 +1,6 @@
 import os, xarray, time, math
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
-from collection_intake.util.fileTest import FileTester
+from stratus_endpoint.util.fileTest import FileTester
 
 base_dirs = "/nfs4m/css/curated01/merra2/data/*"
 suffix = ".nc4"
