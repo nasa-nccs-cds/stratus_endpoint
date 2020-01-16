9a37f89722caf25b93972fe5c98844d959318d48
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-16 13:22:36 -0500

    Update file tests

--- stratus_endpoint/collections/adapt/file_tests/test_cip_files.py
+++ stratus_endpoint/collections/adapt/file_tests/test_cip_files.py
@@ -2,7 +2,7 @@ import os, xarray, time, math
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
 from stratus_endpoint.util.fileTest import FileTester
 
-base_dirs = "/nfs4m/css/curated01/create-ip/data/*"
+base_dirs = "/css/create-ip/data/*"
 suffix = ".nc"
 
 fileTester = FileTester( "cip", suffix, clean=True )
--- stratus_endpoint/collections/adapt/file_tests/test_merra2_files.py
+++ stratus_endpoint/collections/adapt/file_tests/test_merra2_files.py
@@ -2,7 +2,7 @@ import os, xarray, time, math
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Tuple, Optional
 from stratus_endpoint.util.fileTest import FileTester
 
-base_dirs = "/nfs4m/css/curated01/merra2/data/*"
+base_dirs = "/css/merra2/data/*"
 suffix = ".nc4"
 
 fileTester = FileTester( "merra2", suffix )
